c9b790cd3e98f1bd8105e128189e7c5885e83df4
thegreatsantini <lss13@my.fsu.edu>
2018-06-07 11:29:52 -0700

    delete entrys

--- writing_prompts/urls.py
+++ writing_prompts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('signout/', views.signout_view, name='logout'),
     path('signup/', views.signup_view, name='signup'),
     path('profile/<username>', views.profile_view, name="profile"),
-    path('profile/post_entry/', views.post_entry, name="post_entry"),       
+    path('profile/post_entry/', views.post_entry, name="post_entry"),
+    path('profile/delete/<int:entry_id>/', views.delete_entry, name='delete_entry'),      
 ]
 # path('signup/', views.post_cat, name='signup')
