016b5f968d10f66144e2ddc9b9e82c18df8f735d
thegreatsantini <lss13@my.fsu.edu>
2018-07-17 14:53:48 -0700

    removed commented out code

--- writing_prompts/models.py
+++ writing_prompts/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-# class User(models.Model):
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     email = models.EmailField()
-
-#     def __str__(self):
-#         return "%s %s" % (self.first_name, self.last_name)
-
 
 class Entry(models.Model):
     prompt = models.CharField(max_length=200)
